9bceeea6016849f8f0b3b54c7eb4ee9edc7ad82b
Jomxs <161523779+Jomxs@users.noreply.github.com>
2024-11-14 10:21:14 -0300

    Update 14_11.c

--- 2_semester/Lab_Aed_2/Aula/14_11.c
+++ 2_semester/Lab_Aed_2/Aula/14_11.c
@@ -1,8 +1,7 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-
-typedef struct{
+typedef struct {
     int id;
     char cor[20];
     float coordX;
@@ -10,27 +9,59 @@ typedef struct{
 } Ponto;
 
 int lePonto(FILE *f, Ponto *p);
+Ponto cadastraPonto();
+int escrevePonto(FILE *f);
 
-int main(){
+int main() {
     Ponto p;
+    int n;
 
     FILE *f = fopen("pontos.txt", "a+");
-    if(f == NULL){
+    if (f == NULL) {
         printf("Erro ao abrir o arquivo");
         exit(1);
     }
 
-    printf("Pontos eexitentes no arquivo: \n");
+    printf("Pontos existentes no arquivo: \n");
 
-    while(lePonto(f, &p)){
-          printf("%d %s %f %f\n", p.id, p.cor, p.coordX, p.coordY);
-          }
+    while (lePonto(f, &p) > 0) {
+        printf("%d %s %f %f\n", p.id, p.cor, p.coordX, p.coordY);
+    }
+
+    printf("-------------Registrando novos pontos-------------\n");
+    printf("Quantos pontos você deseja registrar? ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        escrevePonto(f);
+    }
     fclose(f);
     return 0;
 }
 
+int lePonto(FILE *f, Ponto *p) {
+    return fscanf(f, "%d %s %f %f", &p->id, p->cor, &p->coordX, &p->coordY);
+}
 
-int lePonto(FILE *f, Ponto *p){
+Ponto cadastraPonto() {
+    Ponto p;
 
-    return fscanf(f, "%d %s %f %f", &p->id, p->cor,&p->coordX, &p->coordY);
+    printf("Digite as informações do ponto: \n");
+    printf("Id: ");
+    scanf("%d", &p.id);
+    getchar();
+    printf("Cor: ");
+    scanf("%s", p.cor);
+    printf("CoordenadaX: ");
+    scanf("%f", &p.coordX);
+    printf("CoordenadaY: ");
+    scanf("%f", &p.coordY);
+
+    return p;
+}
+
+int escrevePonto(FILE *f) {
+    Ponto p = cadastraPonto();
+
+    return fprintf(f, "%d %s %f %f\n", p.id, p.cor, p.coordX, p.coordY);
 }
